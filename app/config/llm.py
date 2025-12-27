320f5ed4ea8e6cd893be22ef12b19d7e29a14081
Pranit Zope <pranit.t.zope@gmail.com>
2025-12-27 19:24:53 +0530

    add logger

--- app/config/llm.py
+++ app/config/llm.py
@@ -2,6 +2,9 @@
 from langchain_openai import ChatOpenAI
 
 from ..constants.openai_constants import OpenaiModels
+from ..utils.logger import get_logger
+
+logger = get_logger()
 
 
 class LLMConfig:
@@ -18,4 +21,9 @@ class LLMConfig:
     
     def get_llm_instance(self) -> ChatOpenAI:
         """Returns an instance of ChatOpenAI with the specified configuration."""
+        logger.info(
+            "OpenAI client initialised with model '%s' and temperature %.2f",
+            self.model_name,
+            self.temperature,
+        )
         return ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
